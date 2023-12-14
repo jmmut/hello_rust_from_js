e3011e2d706c10e79d8ee857a98c10277d2ba53a
jose miguel mut <jomutlo@gmail.com>
2023-12-14 20:09:32 +0100

    trying returning a string, doesn't work

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
+use std::ptr;
+
 #[no_mangle]
 pub fn my_exported_func() -> f64 {
     return 1.618;
+}
+
+#[no_mangle]
+pub fn returning_a_string() -> (*const String, usize) {
+    let s = format!("interpolated {}", my_exported_func());
+    let len = s.len();
+    let ptr = ptr::addr_of!(s);
+    (ptr, len)
 }
